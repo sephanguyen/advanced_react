8229d434eef462d543565fe11377f775437a0819
Pha Nguyen <sephanguyen@gmail.com>
2018-11-12 13:39:52 +0700

    setup build for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,45 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const config = {
   resolve: {
     modules: [path.resolve('./lib'), path.resolve('./node_modules')]
   },
-  entry: ['@babel/polyfill', './lib/renderers/dom.js'],
+  // entry: ['@babel/polyfill', './lib/renderers/dom.js'],
+  entry: {
+    app: ['./lib/renderers/dom.js']
+  },
   output: {
     path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js'
+    filename: '[name].[chunkhash].js'
   },
+
   module: {
-    rules: [{ test: /\.js$/, use: 'babel-loader' }]
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: '!!raw-loader!' + path.join(__dirname, 'views/index.ejs'),
+      filename: 'index.ejs'
+    })
+  ],
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendor',
+          chunks: 'all'
+        }
+      }
+    }
   }
 };
 
